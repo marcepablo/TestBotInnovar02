c1d34a1bc274fa4c680eaa717d63979785b23cab
Marcelo Martinez <marcelo.martinez@innovartecnologias.com>
2017-11-29 12:57:47 +0100

    Transformación hacia "IntermediatorBotSample"

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -1,26 +1,51 @@
+using Microsoft.Bot.Builder.Dialogs;
 using System;
 using System.Threading.Tasks;
-
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Builder.Dialogs;
-using System.Net.Http;
-
+using SimpleEchoBot;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
+    /// <summary>
+    /// Simple echo dialog that tries to connect with a human, if the message contains a certain keyword.
+    /// </summary>
     [Serializable]
     public class EchoDialog : IDialog<object>
     {
+#pragma warning disable 1998
         public async Task StartAsync(IDialogContext context)
         {
-            context.Wait(MessageReceivedAsync);
+            context.Wait(OnMessageReceivedAsync);
         }
+#pragma warning restore 1998
 
-        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        /// <summary>
+        /// Responds back to the sender with the message received or in case the message contains
+        /// a specific keyword, will try to connect with a human (in 1:1 conversation).
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private async Task OnMessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            var message = await argument;
-            await context.PostAsync("You SAIIIDD: " + message.Text);
-            context.Wait(MessageReceivedAsync);
+            IMessageActivity messageActivity = await result;
+            string message = messageActivity.Text;
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                if (message.ToLower().Contains(MessagesController.CommandRequestConnection))
+                {
+                    WebApiConfig.MessageRouterManager.RequestConnection((messageActivity as Activity));
+                }
+                else
+                {
+                    messageActivity = context.MakeMessage();
+                    messageActivity.Text = $"Has dicho: {message}\n\rType \"{Commands.CommandKeyword} {Commands.CommandListOptions}\" to see all command options.\n\rType \"{MessagesController.CommandRequestConnection}\" to initiate conversation with human agent.";
+                    await context.PostAsync(messageActivity);
+                }
+            }
+
+            context.Done(this);
         }
     }
 }
