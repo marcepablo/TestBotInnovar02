74c8a1a67338845f462735ded654b755b114ce24
Marcelo Martinez <marcelo.martinez@innovartecnologias.com>
2017-11-29 10:41:25 +0100

    Cambio prueba

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
         {
             var message = await argument;
-            await context.PostAsync("You said: " + message.Text);
+            await context.PostAsync("You SAIIIDD: " + message.Text);
             context.Wait(MessageReceivedAsync);
         }
     }
